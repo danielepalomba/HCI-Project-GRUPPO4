8e8b4aa7d326c0fc55ddc8b4e04dd85a2e06e5fb
Daniele <palombad46@gmail.com>
2024-07-02 20:40:24 +0200

    pagination & more

--- code/js/rec-stars.js
+++ code/js/rec-stars.js
@@ -1,30 +1,41 @@
-document.getElementById('numberInput').addEventListener('input', function() {
-    const number = this.value;
-    const image = document.getElementById('image');
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('numberInput').addEventListener('input', function() {
 
-    switch (number) {
-        case '1':
-            image.src = '../img/one_star.png';
-            image.alt = 'Descrizione immagine 1';
-            break;
-        case '2':
-            image.src = '../img/two_star.png';
-            image.alt = 'Descrizione immagine 2';
-            break;
-        case '3':
-            image.src = '../img/three_star.png';
-            image.alt = 'Descrizione immagine 3';
-            break;
-        case '4':
-            image.src = '../img/four_star.png';
-            image.alt = 'Descrizione immagine 4';
-            break;
-        case '5':
-            image.src = '../img/five_star.png';
-            image.alt = 'Descrizione immagine 5';
-            break;
-        default:
-            image.src = '';
-            image.alt = 'Nessuna immagine disponibile';
-    }
+        const number = this.value;
+        const image = document.getElementById('image');
+        const text = document.getElementById('rec');
+        const submit = document.getElementById('input-button');
+    
+        if (text != null && text.value.trim() != '') {
+            submit.disabled = false;
+        } else {
+            submit.disabled = true;
+        }
+    
+        switch (number) {
+            case '1':
+                image.src = '../img/one_star.png';
+                image.alt = 'Descrizione immagine 1';
+                break;
+            case '2':
+                image.src = '../img/two_star.png';
+                image.alt = 'Descrizione immagine 2';
+                break;
+            case '3':
+                image.src = '../img/three_star.png';
+                image.alt = 'Descrizione immagine 3';
+                break;
+            case '4':
+                image.src = '../img/four_star.png';
+                image.alt = 'Descrizione immagine 4';
+                break;
+            case '5':
+                image.src = '../img/five_star.png';
+                image.alt = 'Descrizione immagine 5';
+                break;
+            default:
+                image.src = '';
+                image.alt = 'Nessuna immagine disponibile';
+        }
+    });
 });
