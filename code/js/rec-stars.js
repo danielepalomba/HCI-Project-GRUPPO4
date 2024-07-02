6ec821d5915b633ef7941e42587b808fefbd16e2
Daniele <palombad46@gmail.com>
2024-07-02 19:53:56 +0200

    recensioni

---
+++ code/js/rec-stars.js
@@ -0,0 +1,30 @@
+document.getElementById('numberInput').addEventListener('input', function() {
+    const number = this.value;
+    const image = document.getElementById('image');
+
+    switch (number) {
+        case '1':
+            image.src = '../img/one_star.png';
+            image.alt = 'Descrizione immagine 1';
+            break;
+        case '2':
+            image.src = '../img/two_star.png';
+            image.alt = 'Descrizione immagine 2';
+            break;
+        case '3':
+            image.src = '../img/three_star.png';
+            image.alt = 'Descrizione immagine 3';
+            break;
+        case '4':
+            image.src = '../img/four_star.png';
+            image.alt = 'Descrizione immagine 4';
+            break;
+        case '5':
+            image.src = '../img/five_star.png';
+            image.alt = 'Descrizione immagine 5';
+            break;
+        default:
+            image.src = '';
+            image.alt = 'Nessuna immagine disponibile';
+    }
+});
